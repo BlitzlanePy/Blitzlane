e2b13a3e54987b20b962fdb41be3c5fb0d2d9575
Blitzlane <55220598+BlitzlanePy@users.noreply.github.com>
2025-06-03 21:33:34 +0100

    added basic csv reading script by using python

---
+++ CSV_file.py
@@ -0,0 +1,14 @@
+import bot as pd
+
+# loading the dataset
+
+df= pd.read_csv
+
+print(df) #prints Dataset
+# funtions
+print(df.tail())
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.column)
+print(df.shape())
